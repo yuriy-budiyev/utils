2326bf42a80a3ba8f213e0255c43fe0b279bc6e5
Yuriy Budiyev <yuriy.budiyev@gmail.com>
2024-04-29 21:46:16 +0300

    Dev

---
+++ app/build.gradle.kts
@@ -0,0 +1,48 @@
+plugins {
+    alias(libs.plugins.android.application)
+    alias(libs.plugins.kotlin)
+}
+
+android {
+    namespace = "com.github.yuriybudiyev.utils"
+    compileSdk = 34
+
+    defaultConfig {
+        applicationId = "com.github.yuriybudiyev.utils"
+        minSdk = 26
+        targetSdk = 34
+        versionCode = 1
+        versionName = "1.0"
+    }
+
+    buildTypes {
+        release {
+            isMinifyEnabled = false
+
+            proguardFiles(
+                getDefaultProguardFile("proguard-android-optimize.txt"),
+                "proguard-rules.pro"
+            )
+        }
+    }
+
+    compileOptions {
+        sourceCompatibility(libs.versions.java.get())
+        targetCompatibility(libs.versions.java.get())
+    }
+
+    kotlinOptions {
+        jvmTarget = libs.versions.java.get()
+    }
+
+    buildFeatures {
+        buildConfig = true
+    }
+}
+
+dependencies {
+    implementation(libs.bundles.kotlin)
+    implementation(libs.bundles.androidx.annotation)
+    implementation(libs.bundles.androidx.core)
+    implementation(libs.bundles.androidx.collection)
+}
