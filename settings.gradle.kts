656eac1d54772d41b212a10e4f2b5d7e178fb41a
Yuriy Budiyev <yuriy.budiyev@gmail.com>
2024-04-30 17:49:16 +0300

    Dev

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -11,6 +11,8 @@ pluginManagement {
         gradlePluginPortal()
     }
 }
+
+@Suppress("UnstableApiUsage")
 dependencyResolutionManagement {
     repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
     repositories {
@@ -19,5 +21,5 @@ dependencyResolutionManagement {
     }
 }
 
-rootProject.name = "Utils"
+rootProject.name = "utils"
 include(":app")
