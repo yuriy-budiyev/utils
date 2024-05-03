913a98044f4bff3376c6f02bf9579b48451e47c0
Yuriy Budiyev <yuriy.budiyev@gmail.com>
2024-05-03 23:03:37 +0300

    Dev

---
+++ app/src/main/kotlin/com/github/yuriybudiyev/utils/common/Common.kt
@@ -0,0 +1,58 @@
+/*
+ * MIT License
+ *
+ * Copyright (c) 2024 Yuriy Budiyev
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ */
+
+package com.github.yuriybudiyev.utils.common
+
+import android.content.ComponentName
+import android.content.Context
+import android.content.pm.PackageManager
+import kotlin.reflect.KClass
+
+fun Context.toggleComponent(
+    componentClass: KClass<*>,
+    enabled: Boolean,
+) {
+    val appContext = applicationContext
+    appContext.packageManager.setComponentEnabledSetting(
+        ComponentName(
+            appContext,
+            componentClass.java
+        ),
+        if (enabled) PackageManager.COMPONENT_ENABLED_STATE_ENABLED
+        else PackageManager.COMPONENT_ENABLED_STATE_DISABLED,
+        PackageManager.DONT_KILL_APP
+    )
+}
+
+fun Context.checkPackageAvailable(packageName: String): Boolean {
+    try {
+        packageManager.getPackageInfo(
+            packageName,
+            PackageManager.GET_META_DATA
+        )
+        return true
+    } catch (e: PackageManager.NameNotFoundException) {
+        return false
+    }
+}
