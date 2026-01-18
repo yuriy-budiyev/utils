96597e3087bde388c941c978f307a9b8a3b7f604
Yuriy Budiyev <yuriy.budiyev@gmail.com>
2026-01-18 19:08:13 +0300

    Dev

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,4 +1,9 @@
 plugins {
     alias(libs.plugins.android.application) apply false
-    alias(libs.plugins.kotlin) apply false
+}
+
+buildscript {
+    dependencies {
+        classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:${libs.versions.kotlin.asProvider().get()}")
+    }
 }
