954e09e21ac3f9f8d67013e70993cc298972f1ef
Yuriy Budiyev <yuriy.budiyev@yandex.ru>
2026-03-20 12:26:45 +0300

    Dev

---
+++ app/src/main/kotlin/com/github/yuriybudiyev/utils/text/CharSequenceView.kt
@@ -0,0 +1,63 @@
+/*
+ * MIT License
+ *
+ * Copyright (c) 2026 Yuriy Budiyev
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ */
+
+package com.github.yuriybudiyev.utils.text
+
+class CharSequenceView(
+    private val charSequence: CharSequence,
+    private val startIndex: Int,
+    private val endIndex: Int,
+): CharSequence {
+
+    override val length: Int
+        get() = endIndex - startIndex
+
+    override fun get(index: Int): Char {
+        if (index >= length) {
+            throw IndexOutOfBoundsException()
+        }
+        return charSequence[startIndex + index]
+    }
+
+    override fun subSequence(
+        startIndex: Int,
+        endIndex: Int,
+    ): CharSequenceView =
+        CharSequenceView(
+            charSequence = this,
+            startIndex = startIndex,
+            endIndex = endIndex,
+        )
+
+    init {
+        if (startIndex < 0 || endIndex > charSequence.length || startIndex >= endIndex) {
+            throw IllegalArgumentException()
+        }
+    }
+
+    override fun toString(): String =
+        buildString(length) {
+            append(this)
+        }
+}
